747e5f282d2eaa01b747b8fbd69e409258f20deb
ereninal10@gmail.com <ereninal10@gmail.com>
2020-04-23 21:27:53 +0300

    Proje tamamlandı.

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.testapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView txt;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt = (TextView) findViewById(R.id.txtMessage);
+        btn =(Button) findViewById(R.id.btnTikla);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt.setText("Deneme");
+            }
+        });
     }
 }
